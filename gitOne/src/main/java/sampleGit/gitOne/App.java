db8e24598200b863518ed2c722e211dc0e230ff3
susilrout1 <susil.r@echidnainc.com>
2017-06-28 12:21:15 +0530

    test

--- gitOne/src/main/java/sampleGit/gitOne/App.java
+++ gitOne/src/main/java/sampleGit/gitOne/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello GitHub...." );
+        System.out.println( "Hello GitHub.."
+        		+ ".." );
     }
 }
