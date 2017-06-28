d136cfdaed3cca97cef7412e56802dba5aa879b4
susilrout1 <susil.r@echidnainc.com>
2017-06-28 16:25:31 +0530

    SUSIL

--- gitOne/src/main/java/sampleGit/gitOne/App.java
+++ gitOne/src/main/java/sampleGit/gitOne/App.java
@@ -8,7 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello GitHub.."
-        		+ ".." );
-    }
+        for(int i=0;i<=10;i++){
+        	System.out.println(i);
+       }
+        System.out.println("Hello GitHub");
+        }
 }
