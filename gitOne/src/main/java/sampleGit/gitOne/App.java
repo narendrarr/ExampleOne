87953d5d394fadf28dfbf8269adcaba36b36c881
susilrout1 <susil.r@echidnainc.com>
2017-06-29 11:16:01 +0530

    update.

--- gitOne/src/main/java/sampleGit/gitOne/App.java
+++ gitOne/src/main/java/sampleGit/gitOne/App.java
@@ -9,8 +9,9 @@ public class App
     public static void main( String[] args )
     {
     
-    	System.out.println("Hello Git Hub!!!");
-    	System.out.println("Hello Git Hub!!!!");
-    	
+    
+    
+       
+        System.out.println("Hello GitHub Susil");
         }
 }
