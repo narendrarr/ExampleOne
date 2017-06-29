dbd498cc61d12d90773fa4e6646a841d9532e9ef
susilrout1 <susil.r@echidnainc.com>
2017-06-29 14:04:21 +0530

    c

--- gitOne/src/main/java/sampleGit/gitOne/App.java
+++ gitOne/src/main/java/sampleGit/gitOne/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
     
-    	System.out.println("Hello Git Hub!!!");
+    	System.out.println("Hello Git Hub    !!!");
     	
     	
         }
