36b95e53910bdd0792bb2b3a8b9543fd51e32cbd
narendrarr <narendrareddyreddyvari@gmail.com>
2017-06-29 10:59:53 +0530

    Updated.

--- gitOne/src/main/java/sampleGit/gitOne/App.java
+++ gitOne/src/main/java/sampleGit/gitOne/App.java
@@ -9,9 +9,7 @@ public class App
     public static void main( String[] args )
     {
     
-    
-    
-       
-        System.out.println("Hello GitHub Susil");
+    	System.out.println("Hello Git ... I'm neew to this..!!!");
+    	
         }
 }
