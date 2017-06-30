5a47ded57de88432d0a16e0f7215e94a8640d447
susilrout1 <susil.r@echidnainc.com>
2017-06-30 09:40:39 +0530

    New Git

--- gitOne/src/main/java/sampleGit/gitOne/Echidna.java
+++ gitOne/src/main/java/sampleGit/gitOne/Echidna.java
@@ -3,7 +3,7 @@ package sampleGit.gitOne;
 public class Echidna {
 
 	public static void main(String[] args) {
-		System.out.println("Echidna Welcomes you:");
+		System.out.println("Echidna Welcomes you all:");
 		
 
 	}
