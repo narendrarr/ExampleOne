0c9cceb7128e86336e67e17908ebadf5362319cb
susilrout1 <susil.r@echidnainc.com>
2017-06-30 09:46:49 +0530

    W

--- gitOne/src/main/java/sampleGit/gitOne/Git.java
+++ gitOne/src/main/java/sampleGit/gitOne/Git.java
@@ -4,7 +4,7 @@ public class Git {
 
 	public static void main(String[] args) {
 		for(int i=0;i<=10;i++){
-			System.out.println("Hai");
+			System.out.println("Hai ..Welcome to Echidna");
 		}
 
 	}
