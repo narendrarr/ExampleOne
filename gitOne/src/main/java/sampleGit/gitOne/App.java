8903b1b62f88c7346734675a32a4397cdb6b5ab0
susilrout1 <susil.r@echidnainc.com>
2017-07-03 11:18:28 +0530

    Tag

--- gitOne/src/main/java/sampleGit/gitOne/App.java
+++ gitOne/src/main/java/sampleGit/gitOne/App.java
@@ -10,7 +10,7 @@ public class App
     public static void main( String[] args )
     {
     
-    	System.out.println("Welcome to Git Hub..... Bhuvanesh...");
+    	System.out.println("Welcome to Git Hub");
     	
         }
 }
